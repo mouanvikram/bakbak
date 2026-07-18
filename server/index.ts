b90bfeb36d36dcd387e00656eb44edef3c4db555
mouan <mouanvikram@gmail.com>
2026-07-18 22:43:51 +0530

    added express and other libs

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(port,()=>{
+    console.log("app is listening on port http://localhost:"+port)
+})
