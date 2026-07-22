0c59bb974955a8eaca510eba8d4cbfad3140a7b2
mouan <mouanvikram@gmail.com>
2026-07-22 23:00:27 +0530

    reinitalized with new format

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,19 @@
-import "dotenv/config";
+import express from "express";
+import { type Request, type Response} from "express";
+
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/",function(req : Request, res: Response){
+    res.send("Hello World!");
+});
+
+app.listen(PORT,function(){
+    console.log("Server is listening on http://localhost:"+PORT);
+});
---
+++ apps/api/src/auth/server.ts
@@ -0,0 +1,3 @@
+import express from "express";
+import { type Request, type NextFunction, type Response } from "express";
+const router = express.Router();
