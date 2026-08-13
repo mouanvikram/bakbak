26e6e387b33118609c00e9fd35b39fc852b11551
mouan <mouanvikram@gmail.com>
2026-08-13 20:09:20 +0530

    added logo and public files

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,9 @@
+import { LoginPage } from "./features/auth/components/LoginPage";
 
-import './App.css'
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Chat App
-      </h1>
-    </div>
+    <LoginPage />
   );
 }
 
---
+++ apps/web/src/features/auth/components/LoginPage.tsx
@@ -0,0 +1,9 @@
+
+
+export function LoginPage(){
+
+
+    return     <main>
+      <h1>Login</h1>
+    </main>
+}
